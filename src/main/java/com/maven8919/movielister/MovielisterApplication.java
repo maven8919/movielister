f62cc12de070f89ea2a8e56ff9edcc1b037e3696
Levente Bakos <levente_bakos@epam.com>
2017-02-22 15:14:42 +0100

    Initial commit

---
+++ src/main/java/com/maven8919/movielister/MovielisterApplication.java
@@ -0,0 +1,12 @@
+package com.maven8919.movielister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovielisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovielisterApplication.class, args);
+	}
+}
