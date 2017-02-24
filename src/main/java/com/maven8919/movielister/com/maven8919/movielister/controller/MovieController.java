18ef13de046aacf5014dd970715c9a06dcbd7ebf
Levente Bakos <levente_bakos@epam.com>
2017-02-24 16:06:59 +0100

    Add fake movie insertion

--- src/main/java/com/maven8919/movielister/com/maven8919/movielister/controller/MovieController.java
+++ src/main/java/com/maven8919/movielister/com/maven8919/movielister/controller/MovieController.java
@@ -1,13 +1,25 @@
 package com.maven8919.movielister.com.maven8919.movielister.controller;
 
+import com.maven8919.movielister.com.maven8919.movielister.domain.MovieEntity;
+import com.maven8919.movielister.com.maven8919.movielister.service.MovieAdderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MovieController {
 
+    @Autowired
+    private MovieAdderService movieAdderService;
+
     @RequestMapping(value = "/")
     public String movies() {
+        MovieEntity movie = new MovieEntity();
+        movie.setTitle("proba");
+        movie.setRating(22);
+        movie.setWantToWatch(false);
+        movie.setWatched(true);
+        movieAdderService.saveMovie(movie);
         return "movies";
     }
 
---
+++ src/main/java/com/maven8919/movielister/com/maven8919/movielister/service/MovieAdderService.java
@@ -0,0 +1,18 @@
+package com.maven8919.movielister.com.maven8919.movielister.service;
+
+import com.maven8919.movielister.com.maven8919.movielister.com.maven8919.movielister.repository.MovieRepository;
+import com.maven8919.movielister.com.maven8919.movielister.domain.MovieEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieAdderService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public void saveMovie(MovieEntity movie) {
+        movieRepository.save(movie)
+    }
+
+}
