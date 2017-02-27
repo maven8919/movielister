cfceb411cdc9ce51c8c4e4a7cb801bbb7bd98f3d
maven8919 <maven8919@gmail.com>
2017-02-27 23:33:44 +0100

    Remove imdb links from movies.html

--- src/main/java/com/maven8919/movielister/controller/MovieController.java
+++ src/main/java/com/maven8919/movielister/controller/MovieController.java
@@ -19,7 +19,6 @@ public class MovieController {
     @RequestMapping(value = "/")
     public String movies(Model model) {
         model.addAttribute("movies", movieViewFromMovieEntityTransformerService.getMovieViews());
-        model.addAttribute("imdbLinks", ncoreCrawlerService.getImdbLinks());
         return "movies";
     }
 
