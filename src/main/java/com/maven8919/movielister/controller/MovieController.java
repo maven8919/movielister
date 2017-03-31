7119bffa905ab87ff4947118a40783117ec54ad5
maven8919 <maven8919@gmail.com>
2017-03-31 22:46:14 +0200

    Remove unneeded services

--- src/main/java/com/maven8919/movielister/controller/MovieController.java
+++ src/main/java/com/maven8919/movielister/controller/MovieController.java
@@ -1,8 +1,6 @@
 package com.maven8919.movielister.controller;
 
-import com.maven8919.movielister.service.MovieListUpdaterService;
 import com.maven8919.movielister.service.MovieViewFromMovieEntityTransformerService;
-import com.maven8919.movielister.service.NcoreCrawlerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
